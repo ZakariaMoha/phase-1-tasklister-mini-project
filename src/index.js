dcc5efd48f54924465500b44e169f78fddec4a86
zakaria mohamed <zakmi.moha@gmail.com>
2023-10-19 23:35:03 +0300

    Tasklister solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById('create-task-form');
+  const taskList = document.getElementById('tasks');
+
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const taskDescription = document.getElementById('new-task-description').value;
+
+    if (taskDescription) {
+      const newTask = document.createElement('li');
+      newTask.textContent = taskDescription;
+      taskList.appendChild(newTask);
+      document.getElementById('new-task-description').value = '';
+    }
+  });
 });
